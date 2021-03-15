73accffd71c8cba23ee4825a6d21920f1aae640b
Avnerus <avnerus@gmail.com>
2021-03-15 10:48:14 +0200

    Disable request throttling for now

--- modules/tiles/src/tileset/tileset-3d.js
+++ modules/tiles/src/tileset/tileset-3d.js
@@ -85,7 +85,7 @@ const DEFAULT_OPTIONS = {
   modelMatrix: new Matrix4(),
 
   // Set to true to enable experimental request throttling, for improved performance
-  throttleRequests: true,
+  throttleRequests: false,
 
   maximumMemoryUsage: 32,
 
