aaa0232d87da03623f1add4de3a36fe027b9836b
Avnerus <avnerus@gmail.com>
2021-05-29 17:22:21 +0300

    fix in viewDisanceScale support

--- modules/tiles/src/tileset/helpers/tiles-3d-lod.js
+++ modules/tiles/src/tileset/helpers/tiles-3d-lod.js
@@ -137,8 +137,8 @@ export function getTiles3DScreenSpaceError(tile, frameState, useParentLodMetric)
 
   // Avoid divide by zero when viewer is inside the tile
   const distance = Math.max(tile._distanceToCamera, 1e-7);
-  const {height, sseDenominator} = frameState;
-  let error = (lodMetricValue * height) / (distance * sseDenominator);
+  const {height, sseDenominator, viewDistanceScale} = frameState;
+  let error = (lodMetricValue * height * (viewDistanceScale || 1.0)) / (distance * sseDenominator);
 
   error -= getDynamicScreenSpaceError(tileset, distance);
 
